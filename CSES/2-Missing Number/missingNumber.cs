839f3f47626f3ca8b0b90452d1c336a1750169d6
Momen-Assaf <114942380+Momen-Assaf@users.noreply.github.com>
2023-08-04 21:13:21 +0300

    Missing Number

---
+++ CSES/2-Missing Number/missingNumber.cs
@@ -0,0 +1,20 @@
+﻿namespace missingNumber
+{
+    class Solution
+    {
+
+        public static void Main(string[] args)
+        {
+            int[] number = { 1, 2, 3, 4, 5, 7 };
+            int missingNum = 0;
+            Array.Sort(number);
+        
+            for(int i = 0; i < number.Length; i++){
+                if( number[i] != (i + 1)){
+                    missingNum = i+1;
+                }
+            }
+            Console.Write(missingNum);
+        }
+    }
+}
