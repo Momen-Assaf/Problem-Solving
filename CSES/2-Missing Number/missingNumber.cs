72931ad899ebbc17d4116e242ce6c5bfceb61591
Momen-Assaf <114942380+Momen-Assaf@users.noreply.github.com>
2023-08-05 02:58:18 +0300

    Repetition

--- CSES/2-Missing Number/missingNumber.cs
+++ CSES/2-Missing Number/missingNumber.cs
@@ -1,4 +1,4 @@
-﻿namespace missingNumber
+﻿﻿namespace missingNumber
 {
     class Solution
     {
---
+++ CSES/3-Repetitions/Repetition.cs
@@ -0,0 +1,31 @@
+﻿namespace Repetition
+{
+    class Solution
+    {
+
+        public static void Main(string[] args)
+        {
+            string str = "GTTCGGGA";
+            int A = 0, T = 0, C = 0, G = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                switch (str[i])
+                {
+                    case 'A':
+                        A++;
+                        break;
+                    case 'T':
+                        T++;
+                        break;
+                    case 'C':
+                        C++;
+                        break;
+                    default:
+                        G++;
+                        break;
+                }
+            }
+            Console.Write(Math.Max(Math.Max(A,T),Math.Max(C,G)));
+        }
+    }
+}
