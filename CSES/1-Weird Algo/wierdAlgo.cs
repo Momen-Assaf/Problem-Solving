0ccdb1d4aa49ab77ff40238d0704af0612631123
Momen-Assaf <114942380+Momen-Assaf@users.noreply.github.com>
2023-08-04 21:11:17 +0300

    Weird Algo

---
+++ CSES/1-Weird Algo/wierdAlgo.cs
@@ -0,0 +1,26 @@
+﻿namespace WierdAlgo
+{
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            Random random = new();
+            int n = random.Next(10, 20);
+            int t = n;
+            Console.Write(t);
+            while (t != 1)
+            {
+                if ((t % 2) == 0)
+                {
+                    t /= 2;
+                }
+                else
+                {
+                    t = t * 3 + 1;
+                }
+                Console.Write("->" + t);
+            }
+
+        }
+    }
+}
