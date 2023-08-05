2f1b85f0a86523d997cc97fb4bfdaf316658a435
Momen-Assaf <114942380+Momen-Assaf@users.noreply.github.com>
2023-08-05 03:08:07 +0300

    Missing Number(extra is bugged)

--- CSES/2-Missing Number/missingNumber.cs
+++ CSES/2-Missing Number/missingNumber.cs
@@ -1,17 +1,18 @@
 ﻿﻿namespace missingNumber
 {
+    // change code so the strating number is not always 1
     class Solution
     {
 
         public static void Main(string[] args)
         {
-            int[] number = { 1, 2, 3, 4, 5, 7 };
+            int[] number = { 11, 12, 13, 14, 15, 17 };
             int missingNum = 0;
             Array.Sort(number);
         
             for(int i = 0; i < number.Length; i++){
-                if( number[i] != (i + 1)){
-                    missingNum = i+1;
+                if( (number[i] + number[0]) != (i + number[0] + 1)){
+                    missingNum = i + number[0];
                 }
             }
             Console.Write(missingNum);
