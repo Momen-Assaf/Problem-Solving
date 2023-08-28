556578b8f359c0e29a8d82580df8c8064e25a065
Momen-Assaf <114942380+Momen-Assaf@users.noreply.github.com>
2023-08-29 02:35:42 +0300

    Dynamic Programming

---
+++ CSES/Dynamic Programming/Dice Combinations/main.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned ll
+#define pb push_back
+#define pq priority_queue
+#define endl '\n'
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define read_file freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+
+using namespace std;
+
+int n;
+
+void sol(){
+
+}
+int main(){
+
+    cin >> n;
+    sol();
+
+
+}
---
+++ CodeForces/Gym302977/A/main.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned ll
+#define pb push_back
+#define pq priority_queue
+#define endl '\n'
+#define fast ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define read_file freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+
+
+using namespace std;
+
+int sol(ll n){
+    ll f[n+1];
+
+    f[0] = 1;
+    f[1] = 0;
+    for( ll i = 2; i <= n; i++){
+        f[i] = 2*f[i-2];
+    }
+    cout << f[n] << endl;
+}
+
+int main(){
+    fast;
+    ll n;
+    cin >> n;
+    sol(n);
+}
