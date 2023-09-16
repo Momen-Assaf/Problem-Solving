2fddb8a73a7609757416d4235a27d17bcb4cf77f
Momen-Assaf <114942380+Momen-Assaf@users.noreply.github.com>
2023-09-16 20:01:58 +0300

    edit Distance

--- CSES/Dynamic Programming/Edit Distance/main.cpp
+++ CSES/Dynamic Programming/Edit Distance/main.cpp
@@ -7,17 +7,17 @@
 using namespace std;
 
 int main(){
-    fast;
+    //fast;
     string word1,word2;
     cin >> word1 >> word2;
     int n = word1.length();
     int m = word2.length();
     int ed[n+1][m+1];
 
-    for( int i = 0 ; i <= m; i++){
+    for( int i = 0 ; i <= n; i++){
         ed[i][0] = i;
     }
-    for( int i = 1 ; i <= n; i++){
+    for( int i = 1 ; i <= m; i++){
         ed[0][i] = i;
     }
     for( int j = 1; j <= m ; j++){
